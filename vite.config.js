40ac369b4226ee4400f0451fe168a8c4dd81f325
Arif Maulana <69104888+arifmaulana-cf@users.noreply.github.com>
2025-05-31 10:20:26 +0700

    comit vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hayya-app/', 
+  base:  proces.env.VITE_BASE_PATH || '/hayya-app/', 
   plugins: [react(), tailwindcss()],
 
 })
